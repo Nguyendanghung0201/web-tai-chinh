a83590d927c412541d7e644785464416fd0c87d8
danghung <danghung0201@gmail.com>
2023-08-11 21:25:49 +0700

    a

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(function (err, req, res, next) {
     return res.send({ status: false, msg: "error", code: 700, data: err });
 });
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    
